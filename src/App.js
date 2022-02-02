3cfd013fcad79b40073a33851510a9937ff8c37e
Diego Andres Vasquez Avila <davasqueza@unal.edu.co>
2022-02-02 18:00:47 -0500

    Complete demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import {
+    withLicensesProvider,
+    withConfig,
+    withActiveLicenses,
+    withActiveCurriculums,
+    withActiveProducts,
+    withLicenseClient,
+} from "@greatminds/dp-license-react-sdk";
+
+function DisplayConfig({config}) {
+    return (
+        <b>{config.hostURL}</b>
+    );
+}
+
+function DisplayLicenses({licenses}) {
+    return (<b>{JSON.stringify(licenses)}</b>)
+}
+
+function DisplayCurriculums({curriculums}) {
+    return (<b>{JSON.stringify(curriculums)}</b>)
+}
+
+function DisplayProducts({products}) {
+    return (<b>{JSON.stringify(products)}</b>)
+}
+
+class ClientUsage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasAccess: false,
+        };
+    }
+
+    async componentDidMount() {
+        const hasAccess = await this.props.client?.hasALicenseToProduct(['Affirm']);
+        this.setState({ hasAccess });
+    }
+
+    render() {
+        return (
+            <div>
+                <b>Access to Affirm: </b> <span>{this.state.hasAccess ? 'Yes' : 'No'}</span>
+            </div>
+        );
+    }
+}
+
+const LicenseDisplayConfig = withConfig(DisplayConfig);
+const WrappedDisplayLicenses = withActiveLicenses(DisplayLicenses)
+const WrappedDisplayCurriculums = withActiveCurriculums(DisplayCurriculums)
+const WrappedDisplayProducts = withActiveProducts(DisplayProducts)
+const WithLicenseClient = withLicenseClient(ClientUsage);
 
 function App() {
-  return (
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <LicenseDisplayConfig/>
+        <b>----------------------------------------------------------</b>
+        <WrappedDisplayLicenses/>
+        <b>----------------------------------------------------------</b>
+        <WrappedDisplayCurriculums/>
+        <b>----------------------------------------------------------</b>
+        <WrappedDisplayProducts/>
+        <b>----------------------------------------------------------</b>
+        <WithLicenseClient/>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withLicensesProvider({
+    hostURL: 'https://digital.dev.greatminds.dev',
+    districtId: '7300e0f6-a132-42d7-abbb-4c1371a1eec9',
+    user: {}
+})(App);
